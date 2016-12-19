aca7d49e563c227e36c6f7984b1aa6406858b5ab
hello <hello@ifnot.cc>
2016-12-19 16:59:48 +0800

    update and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,32 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   def create
@@ -7,8 +34,13 @@ class ArticlesController < ApplicationController
     # @article = Article.new(params.require(:article).permit(:title, :text))
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # @article.save
+    # redirect_to @article
   end
 
   def show
